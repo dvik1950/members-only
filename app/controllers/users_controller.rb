8388f8bd504113a7750b407834d6fa2753552577
dvik1950 <despic.viktor@gmail.com>
2018-07-12 20:37:50 +0200

    Add login/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user =User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Congratulations, you're finally a member, #{@user[:name]}!"
       redirect_to @user
     else
